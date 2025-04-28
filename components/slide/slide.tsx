488f567a75e1e3b4424361246d3aaf060a039150
jl <jaeli_ottawa@outlook.com>
2025-04-27 23:06:51 -0400

    minor: style slide style

--- components/slide/slide.tsx
+++ components/slide/slide.tsx
@@ -1,54 +1,32 @@
-// @ts-nocheck
-import { useState, useRef, useEffect } from "react";
+//@ts-nocheck
 import theme from "!!raw-loader!./theme.css";
 import Marpit from '@marp-team/marpit'
 interface SlideProps {
     input: string;
     setInput: (value: string) => void;
 }
-console.log("theme", theme)
+
 export function Slide({ input, setInput }: SlideProps) {
-    const marpit = new (Marpit as any)();
-    //     const theme = `
-    // /* @theme example */
-
-    // section {
-    // background-color: #369;
-    // color: #fff;
-    // font-size: 30px;
-    // padding: 40px;
-    // }
-
-    // h1,
-    // h2 {
-    // text-align: center;
-    // margin: 0;
-    // }
-
-    // h1 {
-    // color: #8cf;
-    // }
-    // `
-
-    // 3. Render markdown
-    const markdown = `
-
-# Hello, Marpit!
-
-Marpit is the skinny framework for creating slide deck from Markdown.
-
----
-
-# Ready to convert into PDF!
-
-You can convert into PDF slide deck through Chrome.
-
-`
+    const marpit = new (Marpit as any)({
+        markdown: {
+            // html: true, // Enable HTML tags
+        },
+    });
     marpit.themeSet.default = marpit.themeSet.add(theme)
-    const { html, css } = marpit.render(input)
+    const { html, css, comments } = marpit.render(input)
+    console.log("html", html)
     return (
         <div className="w-[960px] h-[720px] overflow-scroll">
-            <style>{css}</style>
+            <style>
+                {css}
+                {`
+                    .marpit {
+                        display: flex;
+                        flex-direction: column;
+                        gap: 32px;
+                    }
+                `}
+            </style>
             {html ? (
                 <div
                     dangerouslySetInnerHTML={{
