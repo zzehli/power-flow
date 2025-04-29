eadb579c3814b59ffa55c69cb5c5cd42da4a4781
jl <jaeli_ottawa@outlook.com>
2025-04-29 19:11:23 -0400

    feat: update prompt

--- app/api/ai/route.ts
+++ app/api/ai/route.ts
@@ -9,8 +9,8 @@ export async function POST(req: Request) {
 
     const response = await client.chat.completions.create({
         messages: [
-            { role: "system", content: "" },
-            { role: "user", content: "What is the capital of France?" }
+            { role: "system", content: "You are a revealjs/slidev/marp markdown slide generator. Generate beautiful markdowns that uses ### to start a title and split pages with ---. The last page does not need ---. Restrict single page content to 60 words or less. Generate 5 pages at a time. The first page is a title page that only has a # and a ##. You can use emoji to make the presentation interesting if the user question is casual. Don't put emoji in the each page's title and the first page. Use Emoji 16.0 spec like :smile:. You should use math functions if the content needs formula. Use $...$ to render math as inline, and $$...$$ to render as block. Don't use numbered list. Don't wrap the whole output around ```" },
+            { role: "user", content: input }
         ],
         model: "gpt-4o",
         temperature: 1,
@@ -18,7 +18,7 @@ export async function POST(req: Request) {
         top_p: 1
     });
 
-    console.log(response.choices[0].message.content);
+    // console.log(response.choices[0].message.content);
     return new Response(
         JSON.stringify({ message: response.choices[0].message.content }),
         { status: 200, headers: { "Content-Type": "application/json" } }
