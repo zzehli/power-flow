25b7a8b8cbdf9239c1da6450b34fd4abd302e297
jl <jaeli_ottawa@outlook.com>
2025-04-29 17:08:04 -0400

    feat: use marp-core for math and other support

--- components/slide/slide.tsx
+++ components/slide/slide.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import theme from "!!raw-loader!./theme.css";
-import Marpit from '@marp-team/marpit'
+import { Marp as Marpit } from '@marp-team/marp-core'
 import { useEffect, useState } from "react";
 interface SlideProps {
     input: string;
@@ -10,15 +10,6 @@ interface SlideProps {
 
 export function Slide({ input, setInput }: SlideProps) {
     const [content, setContent] = useState({ html: '', css: '' });
-
-    // const marpit = new (Marpit as any)({
-    //     markdown: {
-    //         // html: true, // Enable HTML tags
-    //     },
-    // });
-    // marpit.themeSet.default = marpit.themeSet.add(theme)
-    // const { html, css, comments } = marpit.render(input)
-
     useEffect(() => {
 
         const marpit = new (Marpit as any)({
@@ -30,6 +21,14 @@ export function Slide({ input, setInput }: SlideProps) {
         const { html, css, comments } = marpit.render(input);
         setContent({ html, css });
     }, [input]);
+
+    const htmlFile = `
+<!DOCTYPE html>
+<html><body>
+  <style>${content.css}</style>
+  ${content.html}
+</body></html>
+`
 
     return (
         <div className="w-[960px] h-[720px] overflow-scroll">
